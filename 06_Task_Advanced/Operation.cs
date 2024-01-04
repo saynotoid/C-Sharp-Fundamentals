2eca54152bc65ba5f24a0e30cf49461d2c4bd6bd
Akak <wwwowchik@hotmail.com>
2024-01-04 05:13:05 +0200

    task 6 finished

---
+++ 06_Task_Advanced/Operation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Task_Advanced
+{
+    internal class Operation
+    {
+        public static int GetRectangleArea(int a, int b)
+        {
+            if (a < 0 || b < 0)
+            {
+                throw new ArgumentException();
+            }
+            else if (Int32.MaxValue / a <= b)
+            {
+                throw new OverflowException();
+            }
+            else
+            {
+                return a * b;
+            }
+        }
+
+        public static int TryGetRectangleArea(int a, int b)
+        {
+            try
+            {
+                return GetRectangleArea(a, b);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+    }
+}
---
+++ 06_Task_Advanced/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace _06_Task_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a method for calculating an area of a rectangle
+            // int GetRectangleArea(int a, int b),
+            // which should throw an ArgumentException
+            // if at least one of its arguments is negative and
+            // throw OverflowException if area bigger than Int32.MaxValue.
+            // 
+            // Create TryGetRectangleArea method
+            // which takes two parameters and calls GetRectangleArea
+            // to evaluate an area of a rectangle.
+            // Catch exceptions that can be generated and print the exception message if caught.
+            //
+            // TryGetRectangleArea shouldn't generate any exceptions.
+            // In case when ArgumentException or OverflowException  is caught the method should return 0;
+
+            Console.WriteLine(Operation.GetRectangleArea(-1, 2));
+
+            Console.WriteLine(Operation.TryGetRectangleArea(1, 2));
+
+
+        }
+    }
+}
