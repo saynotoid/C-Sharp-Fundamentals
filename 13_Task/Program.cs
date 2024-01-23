a5fc2ef115266d08b4daa59bef5d68375bbe3858
Akak <wwwowchik@hotmail.com>
2024-01-23 05:42:26 +0200

    start

---
+++ 13_Task/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _13_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Create a class Fruit that contains:
+            //  - fields: name and color,
+            //  - properties for fields,
+            //  - constructor with parameters,
+            //  - virtual methods Input() and Print(), for reading data from the console
+            //  and outputting data to the console (overload the input-output options from the file).
+            //  - override the method ToString().
+            // 2) Create a class Citrus derived from the class Fruit, that contains:
+            // - field - vitamin C (content in grams),
+            // - property,
+            // - constructor with parameters,
+            // - override Input() and Print() methods.
+            // 3) Create a list of fruits and add 5 different fruits and citrus to it.
+            // - Print data about 'yellow' fruits.
+            // - Sort the list of fruits by name and output the result to a file
+            // - Handle exceptions
+            // - Serialize-deserialize the list in Xml/Json format
+            // - Add unit tests for class methods
+
+
+        }
+    }
+}
